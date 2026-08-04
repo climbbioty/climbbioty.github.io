564c906760e63736b0f143b08e54431c4cf51db3
climbbioty <climbbioty@icloud.com>
2026-08-04 16:59:14 -0600

    Add watchPrompt function to monitor room prompts

--- js/multiplayer.js
+++ js/multiplayer.js
@@ -1,1 +1,16 @@
-//multiplayer
+function watchPrompt(){
+
+    onValue(
+        ref(database, "rooms/" + currentRoom + "/prompt"),
+        (snapshot)=>{
+
+            if(snapshot.exists()){
+
+                promptBox.textContent = snapshot.val();
+
+            }
+
+        }
+    );
+
+}
