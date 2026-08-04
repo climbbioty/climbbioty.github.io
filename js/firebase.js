9df63b4c0affdb877e6c79f06eca77432a9b1fb8
climbbioty <climbbioty@icloud.com>
2026-08-04 16:55:10 -0600

    Initialize Firebase app and export database functions

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,25 @@
-//firebaseness
+import { initializeApp } from "...";
+
+import {
+    getDatabase,
+    ref,
+    set,
+    get,
+    child,
+    onValue
+} from "...";
+
+const firebaseConfig = { ... };
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export {
+    database,
+    ref,
+    set,
+    get,
+    child,
+    onValue
+};
