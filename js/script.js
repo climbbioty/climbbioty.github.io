b26fa16aff11807e0bb7aeb859447165fcf0cf29
climbbioty <climbbioty@icloud.com>
2026-08-06 00:22:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,32 +25,3 @@ document.getElementById("roomDisplay").textContent =
     "Room Code: " + roomCode;
 
 });
-
-document.getElementById("joinRoomButton")
-.addEventListener("click", async () => {
-
-    const code =
-        document.getElementById("roomInput")
-        .value
-        .toUpperCase();
-
-    const snapshot = await get(
-        child(ref(database), "rooms/" + code)
-    );
-
-    if(snapshot.exists()){
-
-        currentRoom = code;
-
-        watchPrompt();
-
-        document.getElementById("roomDisplay").textContent =
-            "Joined Room: " + code;
-
-    }else{
-
-        alert("Room not found.");
-
-    }
-
-});
