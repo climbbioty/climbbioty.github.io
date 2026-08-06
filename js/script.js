69ce3a562b06787503122dd516992163e6e9bdef
climbbioty <climbbioty@icloud.com>
2026-08-06 00:37:34 -0600

    Import watchPrompt from game.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import {
     child,
     onValue
 } from "./firebase.js";
-import {} from "./game.js";
+import { watchPrompt} from "./game.js";
 import {prompts} from "./prompts.js";
 import { words } from "./words.js";
 import {setupRooms} from "./room.js";
