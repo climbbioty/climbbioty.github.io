796629cfd5d74fcb277f9d47d0e9df25607ec807
climbbioty <climbbioty@icloud.com>
2026-08-06 00:34:29 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,21 +7,6 @@ import {
     onValue
 } from "./firebase.js";
 import {} from "./game.js";
-import {} from "./prompts.js";
-import {} from "./words.js";
+import {prompts} from "./prompts.js";
+import { words } from "./words.js";
 import {setupRooms} from "./room.js";
-
-const wordButton = document.getElementById("wordButton");
-
-wordButton.addEventListener("click", () => {
-
-    generateWords();
-
-});
-
-watchPrompt();
-
-document.getElementById("roomDisplay").textContent =
-    "Room Code: " + roomCode;
-
-});
