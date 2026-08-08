c4bea84d8ac433118988a3155d116c719a1b0763
climbbioty <climbbioty@icloud.com>
2026-08-08 01:54:08 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -11,6 +11,8 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+import {currentRoom} from "./room.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA7PpddeYPWBczMv7z-VHZ0esFwkaBNBms",
   authDomain: "ransomnoteish.firebaseapp.com",
